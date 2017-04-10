a776f0510d579ea3b0594f302f1f9079cabc6cc9
Lei Ma <LMa2@loyalty.com>
2017-04-10 14:07:54 -0400

    add some files

---
+++ test_om_pytest.py
@@ -0,0 +1,15 @@
+'''
+Test other_math with pytest
+To run tests : py.test             test_om_pytest.py
+          or : python -m pytest    test_om_pytest.py
+Verobse (-v) : py.test -v          test_om_pytest.py
+          or : python -m pytest -v test_om_pytest.py
+'''
+
+from other_math import multiply
+
+def test_numbers_3_4():
+    assert multiply(3,4) == 12 
+
+def test_strings_a_3():
+    assert multiply('a',3) == 'aaa' 
