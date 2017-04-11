6b357dec0075f5af91ec017cbeab38f6e73d27c8
Lei Ma <LMa2@loyalty.com>
2017-04-10 21:26:34 -0400

    fix coding stardards

--- test_om_pytest.py
+++ test_om_pytest.py
@@ -9,7 +9,9 @@ Verobse (-v) : py.test -v          test_om_pytest.py
 from other_math import multiply
 
 def test_numbers_3_4():
-    assert multiply(3,4) == 12 
+    """Test multiply in numbers"""
+    assert multiply(3, 4) == 12
 
 def test_strings_a_3():
-    assert multiply('a',3) == 'aaa' 
+    """Test multiply in strings"""
+    assert multiply('a', 3) == 'aaa'
